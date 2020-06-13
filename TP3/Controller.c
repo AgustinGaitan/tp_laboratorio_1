f6b87abf25ff6775c453100604a0b0a8949a0178
jorge <asd@hotmail.com>
2020-06-13 14:42:53 -0300

    Trabajo Practico N3

--- TP3/Controller.c
+++ TP3/Controller.c
@@ -6,16 +6,16 @@
 #include "utn.h"
 #include "menus.h"
 
-int controller_loadFromText(char* path , LinkedList* pArrayListEmployee)
+int controller_loadFromText(char* path , LinkedList* pArrayListEmployee) //funcion que se llama desde el main
 {
 	int todoOk = 1;
-	FILE* pf =  fopen(path,"r");
+	FILE* pf =  fopen(path,"r");                    // se abre la file para lectura 'r'
 	system("cls");
 
 		 if(pf != NULL)
 		 {
 			 parser_EmployeeFromText(pf,pArrayListEmployee); //se parsea de string a su debido tipo
-			 printf("Archivo encontrado.\n\n");
+			 printf("Archivo encontrado.\n\n"); //todook
 			 todoOk = 0;
 
 		 }
@@ -36,14 +36,18 @@ int controller_loadFromBinary(char* path , LinkedList* pArrayListEmployee)
 {
 
 	int todoOk = 1;
+	system("cls");
 
-		FILE* pf;
 
-		pf = fopen(path, "rb");
+		FILE* pf = fopen(path, "rb"); // se abre el archivo con opcion read binary "rb"
+
+
+
+
 
 		if(pf != NULL)
 		{
-			parser_EmployeeFromBinary(pf, pArrayListEmployee);
+			parser_EmployeeFromBinary(pf, pArrayListEmployee);       //se parsea
 
 
 				printf("Archivo encontrado.\n");
@@ -158,11 +162,11 @@ int controller_editEmployee(LinkedList* pArrayListEmployee)
 			if((utn_getEntero(&auxId, 2, "Ingrese el id del empleado que quiere modificar: ", "Error. Id no encontrado\n", 0, 150000 )) == 0)
 			{
 				indice = findEmployeeById(pArrayListEmployee, auxId);
-				printf("%d", indice);
+				//printf("%d", indice);
 				if(indice >= 0)
 				{
 					printf("Id    Nombre   Horas Trabajadas   Sueldo\n\n");
-					printEmployee(pArrayListEmployee, indice);
+					printEmployee(pArrayListEmployee, indice);            //muestra al empleado que se va a modificar
 
 					printf("\nConfirma la modificacion del empleado ?'s' para aceptar, 'n' para negar ");
 					fflush(stdin);
@@ -176,40 +180,40 @@ int controller_editEmployee(LinkedList* pArrayListEmployee)
 							switch(menuModificacion())
 							{
 
-								case 1:
+								case 1:				//modificacion de id
 									system("cls");
 									pEmpleado = ll_get(pArrayListEmployee,indice);
 									printf("Ingrese el nuevo id: ");
 									scanf("%d", &nuevoId);
-									employee_setId(pEmpleado, nuevoId);
+									employee_setId(pEmpleado, nuevoId);  //se setea el nuevo id
 									printf("Id modificado.\n");
 
 									break;
-								case 2:
+								case 2:              //modificacion de nombre
 									system("cls");
 									pEmpleado = ll_get(pArrayListEmployee,indice);
 									printf("Ingrese el nuevo nombre: ");
 									fflush(stdin);
 									gets(nuevoNombre);
-									employee_setNombre(pEmpleado, nuevoNombre);
+									employee_setNombre(pEmpleado, nuevoNombre);  //se le setea el nuevo nombre
 									printf("Nombre modificado.\n");
 
 									break;
-								case 3:
+								case 3:                   //modificacion de horas
 										system("cls");
 										pEmpleado = ll_get(pArrayListEmployee,indice);
 										printf("Ingrese las nuevas horas: ");
 										scanf("%d", &nuevasHoras);
-										employee_setHorasTrabajadas(pEmpleado, nuevasHoras);
+										employee_setHorasTrabajadas(pEmpleado, nuevasHoras);     //se le setean las nuevas horas
 										printf("Horas modificadas.\n");
 
 									break;
-								case 4:
+								case 4:                                     //modificacion de sueldo
 
 									pEmpleado = ll_get(pArrayListEmployee,indice);
 									printf("Ingrese el nuevo sueldo: ");
 									scanf("%d", &nuevoSueldo);
-									employee_setSueldo(pEmpleado, nuevoSueldo);
+									employee_setSueldo(pEmpleado, nuevoSueldo);       //se le setea el nuevo sueldo
 									printf("Sueldo modificado.\n");
 									break;
 								case 5:
@@ -256,7 +260,7 @@ int controller_removeEmployee(LinkedList* pArrayListEmployee)
 
 			if((utn_getEntero(&auxId, 2, "Ingrese el id del empleado que le quiere dar de baja.\n", "Error. Id no encontrado", 0, 150000 ) == 0))
 			{
-				indice = findEmployeeById(pArrayListEmployee, auxId);
+				indice = findEmployeeById(pArrayListEmployee, auxId);  //encuentra el indice respectivo al id ingresado
 
 				if(indice >= 0)
 				{
@@ -268,8 +272,8 @@ int controller_removeEmployee(LinkedList* pArrayListEmployee)
 					scanf("%c", &confirmar);
 					if(confirmar == 's')
 					{
-						ll_remove(pArrayListEmployee, indice);
-						employee_delete(pEmpleado);
+						ll_remove(pArrayListEmployee, indice);  //se borra
+						employee_delete(pEmpleado);  //se le saca el espacio de la memoria dinamica
 						printf("Baja exitosa.\n");
 
 						todoOk = 0;
@@ -295,16 +299,16 @@ int controller_ListEmployee(LinkedList* pArrayListEmployee)
 {
 
 	int todoOk = 1;
-	int tam = ll_len(pArrayListEmployee);
+	int tam = ll_len(pArrayListEmployee);  //se le asigna el tamaño
 	system("cls");
 
 		if(pArrayListEmployee != NULL)
 		{
-			printf("\nId     Nombre      Horas Trabajadas    Sueldo\n\n");
+			printf("Id     Nombre      Horas Trabajadas    Sueldo\n\n");
 			for(int i = 0; i < tam; i++)
 			{
 
-					printEmployee(pArrayListEmployee, i);
+					printEmployee(pArrayListEmployee, i);  //muestra a los empleados uno por uno
 
 			}
 		}
@@ -327,20 +331,24 @@ int controller_sortEmployee(LinkedList* pArrayListEmployee)
 	int opcion;
 
 	system("cls");
-	utn_getEntero(&opcion, 2, "Ingrese si quiere ordenar por nombre(1), o por id (2):   ", "Error. Ingrese un numero valido.\n", 1, 2);
+	utn_getEntero(&opcion, 2, "Ingrese si quiere ordenar por nombre(1), o por id (2):   ", "Error. Ingrese un numero valido.\n", 1, 2); //eleccion
 		switch(opcion)
 		{
-		case 1:
+		case 1:  //primer opcion
 			system("cls");
 			utn_getEntero(&orden, 2, "Ingrese el orden en el que quiere que se \nacomoden los nombres.Recuerde que este proceso\ntarda unos segundos. (1 ascendente o 0 descendente): ", "Error. Ingrese un numero valido\n", 0, 1);
 
 			ll_sort(pArrayListEmployee, sortEmployeeNombre, orden);
+
+			//se ordenan por nombre
 			break;
-		case 2:
+		case 2:  //segunda opcion
 			system("cls");
 			utn_getEntero(&orden, 2, "Ingrese el orden en el que quiere que se \nacomoden los Id.Recuerde que este proceso\ntarda unos segundos. (1 ascendente o 0 descendente): ", "Error. Ingrese un numero valido\n", 0, 1);
 
 			ll_sort(pArrayListEmployee, sortEmployeeId,orden);
+
+			//se ordenan por id
 			break;
 
 		}
@@ -361,36 +369,37 @@ int controller_saveAsText(char* path , LinkedList* pArrayListEmployee)
 	int todoOk = 1;
 
 		FILE* pf;
-		int auxId;
 		char auxNombre[20];
+		int auxId;
 		int auxHoras;
 		int auxSueldo;
-		int tam = ll_len(pArrayListEmployee);
+		int tam = ll_len(pArrayListEmployee); // se le asigna el tamaño
 		Employee* pEmpleado;
 
-		if(pArrayListEmployee != NULL && path != NULL)
-		{
-			pf = fopen(path, "w");
+
+			pf = fopen(path, "w");                   //
+
+
 			if(pf != NULL)
 			{
 
-				for(int i = 0; i < tam; i++)
+				for(int i = 0; i < tam; i++)  //recorre para cada empleado
 				{
-					pEmpleado = ll_get(pArrayListEmployee, i);
+					pEmpleado = ll_get(pArrayListEmployee, i);  //obtiene cada empleado mediante el indice
 
 					if((employee_getId(pEmpleado, &auxId)==0) &&
 					   (employee_getNombre(pEmpleado, auxNombre)==0) &&
-					   (employee_getHorasTrabajadas(pEmpleado, &auxHoras)==0) &&
+					   (employee_getHorasTrabajadas(pEmpleado, &auxHoras)==0) &&  // si esta todo ok pasa a imprimir los empleados
 					   (employee_getSueldo(pEmpleado, &auxSueldo)==0))
 					{
-						fprintf(pf, "%d,%s,%d,%d\n", auxId, auxNombre, auxHoras, auxSueldo);
+						fprintf(pf, "%d,%s,%d,%d\n", auxId, auxNombre, auxHoras, auxSueldo);  //imprime a los empleados en el archivo
 						todoOk = 0;
 					}
 				}
 				fclose(pf);
 				printf("Archivo de texto guardado.\n");
 			}
-		}
+
 
 	    return todoOk;
 }
@@ -401,22 +410,26 @@ int controller_saveAsBinary(char* path , LinkedList* pArrayListEmployee)
 	int todoOk = 1;
 		FILE* pf;
 		Employee* pEmpleado;
-		int tam = ll_len(pArrayListEmployee);
+		int tam = ll_len(pArrayListEmployee); //tamaño
 
-		if(path != NULL && pArrayListEmployee)
-		{
-			pf = fopen(path, "wb");
 
-				for(int i = 0; i < tam; i++)
+			pf = fopen(path, "wb");      //abre el archivo con opcion de escritura
+
+				for(int i = 0; i < tam; i++) //recorre a los empleados con intencion de escribirlos
 				{
 					pEmpleado = (Employee*) ll_get(pArrayListEmployee, i);
-					fwrite(pEmpleado, sizeof(Employee), 1, pf);
+					fwrite(pEmpleado, sizeof(Employee), 1, pf);          //los escribe
+
+
 				}
 
-			printf("Archivo binario guardado.\n");
-			fclose(pf);
+				printf("Archivo binario guardado.\n");
+
+			fclose(pf); //se cierra el archivo
+
+
 			todoOk = 0;
-		}
+
 
 	    return todoOk;
 }
